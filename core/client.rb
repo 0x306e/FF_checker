0081be266a4089adaf078c87fd8c1ac9dfc1ef2f
0x306e <3so.d.na@gmail.com>
2017-12-28 02:19:40 +0900

    delete module

--- core/client.rb
+++ core/client.rb
@@ -1,10 +1,8 @@
-module FF_Checker
-  class Client
-    def initialize(data)
-      @consumer_key = data.consumer_key
-      @consumer_secret = data.consumer_secret
-      @access_token = data.access_token
-      @access_token_secret = data.access_token_secret
-    end
+class Client
+  def initialize(data)
+    @consumer_key = data.consumer_key
+    @consumer_secret = data.consumer_secret
+    @access_token = data.access_token
+    @access_token_secret = data.access_token_secret
   end
 end
--- core/configger.rb
+++ core/configger.rb
@@ -1,13 +1,11 @@
 require 'yaml'
 
-module FF_checker
-  class Configger
-    def initialize(uid)
-      @file = File.expand_path('./data/' + uid + '.yml', __FILE__)
-    end
+class Configger
+  def initialize(uid)
+    @file = File.expand_path('./data/' + uid + '.yml', __FILE__)
+  end
 
-    def load
-      return YAML.load_file(@file)
-    end
+  def load
+    return YAML.load_file(@file)
   end
 end
