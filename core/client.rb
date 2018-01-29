e6b2bb9ff74f7c6c016b3247937115253c5a5306
0x306e <3so.d.na@gmail.com>
2018-01-29 09:43:41 +0900

    delete

--- core/client.rb
+++
@@ -1,8 +0,0 @@
-class Client
-  def initialize(data)
-    @consumer_key = data.consumer_key
-    @consumer_secret = data.consumer_secret
-    @access_token = data.access_token
-    @access_token_secret = data.access_token_secret
-  end
-end
