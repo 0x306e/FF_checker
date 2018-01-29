23834a71dd30bc092ed2bf127bbf7da0093ef198
0x306e <3so.d.na@gmail.com>
2018-01-29 11:26:50 +0900

    jsonのフォーマット変更

--- core/configger.py
+++ core/configger.py
@@ -3,10 +3,10 @@ import json
 
 class Configger:
     def __init__(self, path):
-        self.__file = open(path)
-        self.conf = json.load(open(self.__file))['user']
-        self.__file.close()
-        self.consumer_key = self.conf['consumer_key']
-        self.consumer_secret = self.conf['consumer_secret']
-        self.access_token = self.conf['access_token']
-        self.access_token_secret = self.conf['access_token_secret']
+        self.file = open(path, 'r')
+        self.conf = json.load(self.file)
+        self.file.close()
+        self.consumer_key = self.conf['user']['consumer_key']
+        self.consumer_secret = self.conf['user']['consumer_secret']
+        self.access_token = self.conf['user']['access_token']
+        self.access_token_secret = self.conf['user']['access_token_secret']
