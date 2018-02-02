37452746496bade6875747998d94e908956d43eb
0x306e <3so.d.na@gmail.com>
2018-02-02 11:37:05 +0900

    メッセージ生成の変更, メンバのprivate化

--- core/checker.py
+++ core/checker.py
@@ -3,60 +3,64 @@ import copy
 
 class Checker:
     def __init__(self, previous, currently):
-        self.previous = previous
-        self.currently = currently
-        self.diff = {
+        self.__previous = previous
+        self.__currently = currently
+        self.__diff = {
             'added': {},
             'removed': {},
             'screen_name_changed': {}
         }
 
     def __get_diff(self):
-        previous_dict = copy.deepcopy(self.previous)
-        now_dict = copy.deepcopy(self.currently)
+        previous_dict = copy.deepcopy(self.__previous)
+        now_dict = copy.deepcopy(self.__currently)
 
         for j, k in now_dict.items():
             str_j = str(j)
             if str_j in previous_dict:
                 if previous_dict[str_j] != k:
-                    self.diff['screen_name_changed'][str_j] = {
+                    self.__diff['screen_name_changed'][str_j] = {
                         'previous': previous_dict[str_j],
                         'currently': k
                     }
                 del previous_dict[str_j]
             else:
-                self.diff['added'][str_j] = k
-        self.diff['removed'] = previous_dict
-        return self.diff
+                self.__diff['added'][str_j] = k
+        self.__diff['removed'] = previous_dict
+        return self.__diff
 
     def create_message_follower(self):
         messages = []
         self.__get_diff()
-        for i, j in self.diff.items():
+        for i, j in self.__diff.items():
             if i == 'added':
                 for k, l in j.items():
-                    messages.append(f"@{l} has followed you.")
+                    messages.append(f"@{l} ")
+                messages.append('has followed you. ')
             if i == 'removed':
                 for k, l in j.items():
-                    messages.append(f"@{l} has removed you.")
+                    messages.append(f"@{l} ")
+                messages.append('has removed you. ')
             if i == 'screen_name_changed':
                 for k, l in j.items():
-                    messages.append(f"@{l['currently']} has changed name from @{l['previous']}")
-        message = '\n'.join(messages)
+                    messages.append(f"@{l['currently']} has changed name from @{l['previous']}. ")
+        message = ' '.join(messages)
         return message
 
     def create_message_following(self):
         messages = []
         self.__get_diff()
-        for i, j in self.diff.items():
+        for i, j in self.__diff.items():
             if i == 'added':
+                messages.append('you have followed ')
                 for k, l in j.items():
-                    messages.append(f"you have followed @{l}.")
+                    messages.append(f"@{l} ")
             if i == 'removed':
+                messages.append('you have removed ')
                 for k, l in j.items():
-                    messages.append(f"you have removed @{l}.")
+                    messages.append(f"@{l} ")
             if i == 'screen_name_changed':
                 for k, l in j.items():
-                    messages.append(f"@{l['currently']} has changed name from @{l['previous']}")
-        message = '\n'.join(messages)
+                    messages.append(f"@{l['currently']} has changed name from @{l['previous']}. ")
+        message = ' '.join(messages)
         return message
